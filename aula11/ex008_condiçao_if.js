74cad205967dcb941a52d1f24210e4ad3ace297c
Rone Fernando <ronnychack@yahoo.com.br>
2023-07-02 20:01:46 -0300

    exec update.

---
+++ aula11/ex008_condiçao_if.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro e ${vel} km/h`)
+if (vel > 60){
+    console.log(`Você ultrapassou velocidade permitida.MULTADO`)
+}
+console.log(`Digija com cuidado , volte sempre`)
