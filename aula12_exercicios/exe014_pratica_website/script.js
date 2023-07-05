8e8cf23175f6d3c3e4430429f506a93b39c640b9
Rone Fernando <ronnychack@yahoo.com.br>
2023-07-05 01:33:38 -0300

    ok

--- aula12_exercicios/exe014_pratica_website/script.js
+++ aula12_exercicios/exe014_pratica_website/script.js
@@ -8,9 +8,12 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`;
     if (hora >= 0 && hora < 12) {
         img.src = 'manha.jpg';
+        document.body.style.background ='#e2cd9f';
     } else if (hora < 18) {
         img.src = 'tarde.jpg';
+        document.body.style.background ='#b9846f';
     } else {
         img.src = 'noite.jpg';
+        document.body.style.background ='#515154';
     }
 }
