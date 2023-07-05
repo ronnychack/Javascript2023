f4ec12bb7692ef318d74ff608f6da2085450af0a
Rone Fernando <ronnychack@yahoo.com.br>
2023-07-05 01:19:32 -0300

    update corrigindo bug nao trocar imagem.
    tive que colocar imagem default para funcionar manha , tarde, noite, talvez para nao gerar conflitos nao sei ao certo, mas resolveu problema.

--- aula12_exercicios/exe014_pratica_website/script.js
+++ aula12_exercicios/exe014_pratica_website/script.js
@@ -1,19 +1,16 @@
-function carregar(){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas`//ate aqui esta correto
-
-
-/*Carrega no corpo texto da id= msg e também informa a hora do sistema(no caso meu pc) */
-    if(hora >= 0 && hora < 12){
-        img.scr = 'manha.jpg'
-
-    }else if(hora<=18){
-        img.scr = 'tarde.jpg'
-    }else{
-        img.scr = 'noite.jpg'
+function carregar() {
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    //comentario pode prejudicar o codigo?
+    
+    msg.innerHTML = `Agora são ${hora} horas.`;
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manha.jpg';
+    } else if (hora < 18) {
+        img.src = 'tarde.jpg';
+    } else {
+        img.src = 'noite.jpg';
     }
 }
-
