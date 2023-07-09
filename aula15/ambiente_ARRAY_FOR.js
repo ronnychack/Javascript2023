d92f6d021e8d6d865cdf2596b9e73aea4244775b
Rone Fernando <ronnychack@yahoo.com.br>
2023-07-09 19:55:53 -0300

    update exercicio array

---
+++ aula15/ambiente_ARRAY_FOR.js
@@ -0,0 +1,4 @@
+let num = [1,3,8,4,1,12]
+for (let pos = 0;pos < num.length;pos++){
+    console.log(`A posiçao ${pos} tem o valor ${num[pos]}`)
+}
---
+++ aula15/ambiente_array.js
@@ -0,0 +1,7 @@
+let num = [0,1,2,3,4,5]
+num.sort()//coloca vetor em ordem
+num.push(8)//acrecenta valor ao elemento no final
+num[7] =10// acrescenta valor no elemento 7 valor de 10
+console.log(`Nosso vetor é o ${num[5]}`)
+console.log(`Vetor tem ${num.length} posiçoes`) 
+console.log(num)
